eeb00d7f2caf8269d7382774c2138edb543a0c9d
fatihcandev <contact@fatihcan.dev>
2021-02-08 15:24:14 +0300

    changes in components for tests

--- src/components/PickerToggler/PickerToggler.tsx
+++ src/components/PickerToggler/PickerToggler.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleProp, TextStyle, TouchableOpacity, View, ViewStyle } from 'react-native';
 
 import { Flag } from '../Flag';
@@ -12,8 +12,7 @@ import styles from './styles';
 interface IPickerTogglerProps {
   selectedCode?: string;
   flag?: any;
-  isPickerOpen: boolean;
-  onPickerToggle: () => void;
+  onPickerToggle: (isOpen: boolean) => void;
   containerStyle?: StyleProp<ViewStyle>;
   textStyle?: StyleProp<TextStyle>;
 }
@@ -21,26 +20,30 @@ interface IPickerTogglerProps {
 const PickerToggler: React.FC<IPickerTogglerProps> = ({
   selectedCode,
   flag,
-  isPickerOpen,
   onPickerToggle,
   containerStyle,
   textStyle,
 }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const arrowDownStyle: StyleProp<ViewStyle> = {
     transform: [
       {
-        rotate: isPickerOpen ? '180deg' : '0deg',
+        rotate: isOpen ? '180deg' : '0deg',
       },
     ],
   };
 
+  useEffect(() => {
+    onPickerToggle(isOpen);
+  }, [isOpen, onPickerToggle]);
+
   return (
     <View style={containerStyle}>
-      <TouchableOpacity onPress={onPickerToggle}>
+      <TouchableOpacity onPress={() => setIsOpen(state => !state)} testID="toggler-button">
         <View style={styles.innerContainer}>
           <Flag flag={flag} />
           <StyledText style={textStyle}>{`+${selectedCode}`}</StyledText>
-          <View style={arrowDownStyle}>
+          <View style={arrowDownStyle} testID="arrow-down-icon">
             <SvgArrowDown color={colors.black} width={18} height={18} />
           </View>
         </View>

2bc3cc4eb7edca8d57284a74e56e775182b9b34e
loneddonkey <a.tvd@outlook.com>
2022-05-30 12:54:27 +0300

    fix: text color fixed

--- src/components/StyledText/styles.ts
+++ src/components/StyledText/styles.ts
@@ -1,10 +1,8 @@
 import { StyleSheet } from 'react-native';
-import { colors } from '../../theme';
 
 export default StyleSheet.create({
   text: {
     fontSize: 16,
     lineHeight: 24,
-    color: colors.black,
   },
 });
